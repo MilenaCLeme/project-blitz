052259bb5e9e1918a8397c93c1011de03dff7b8a
MilenaCLeme <milenaleme4@hotmail.com>
2022-02-17 01:58:55 -0300

    fix: insert func update in models

--- back-end/models/connection.js
+++ back-end/models/connection.js
@@ -19,4 +19,4 @@ const connection = () => (
         })
 );
 
-module.exports = connection;
+module.exports = {connection};
--- back-end/models/dolistmodel.js
+++ back-end/models/dolistmodel.js
@@ -1,7 +1,7 @@
 const connect = require('./connection');
 
 const create = async (activity) => {
-  const db = await connect();
+  const db = await connect.connection();
   const {insertedId} = await db.collection('list').insertOne({activity});
   return {
     id: insertedId,
@@ -10,12 +10,28 @@ const create = async (activity) => {
 };
 
 const findAll = async () => {
-  const db = await connect();
+  const db = await connect.connection();
   const result = await db.collection('list').find().toArray();
   return result;
 };
 
+const update = async (id, activity) => {
+  const db = await connect.connection();
+  await db.collection('list')
+      .updateOne({id}, {$set: {activity}});
+  return {
+    id,
+    activity,
+  };
+};
+
+const delete = async (id) => {
+
+}
+
 module.exports = {
   create,
   findAll,
+  update,
+  delete
 };
