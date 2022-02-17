62a914a30e76b257ad2a9463cdd03cc151695df8
MilenaCLeme <milenaleme4@hotmail.com>
2022-02-17 01:57:10 -0300

    test: insert delete and update in model test

--- back-end/tests/models/dolistmodel.test.js
+++ back-end/tests/models/dolistmodel.test.js
@@ -1,7 +1,7 @@
 const sinon = require('sinon');
 const {expect} = require('chai');
 const {MongoClient} = require('mongodb');
-const {getConnection} = require('../mongomockconnection');
+const {getConnection} = require('./mongomockconnection');
 
 const listModel = require('../../models/dolistmodel');
 
@@ -18,8 +18,7 @@ describe('Insere um nova lista no BD', () => {
   });
 
   afterAll(async () => {
-    await connectMock.db('blitz').collection('list').drop();
-    MongoClient.connect.restore();
+    sinon.restore();
   });
 
   describe('Quando for um sucesso', () => {
@@ -38,6 +37,17 @@ describe('Insere um nova lista no BD', () => {
 });
 
 describe('gerar a lista de todas as tarefas', () => {
+  let connectMock;
+
+  beforeAll(async () => {
+    connectMock = await getConnection();
+    sinon.stub(MongoClient, 'connect').resolves(connectMock);
+  });
+
+  afterAll(async () => {
+    sinon.restore();
+  });
+
   describe('Quando for um sucesso', () => {
     it('Retorna um array', async () => {
       const result = await listModel.findAll();
@@ -47,3 +57,70 @@ describe('gerar a lista de todas as tarefas', () => {
   });
 });
 
+
+describe('Atualizar a tarefa no banco de dados', () => {
+  let connectMock;
+
+  const activityOld = 'entrevista amanhã as 15 horas';
+
+  const activityNew = 'sair com amigos';
+
+  beforeAll(async () => {
+    connectMock = await getConnection();
+    sinon.stub(MongoClient, 'connect').resolves(connectMock);
+  });
+
+  afterAll(async () => {
+    sinon.restore();
+  });
+
+  describe('Quando for um sucesso', () => {
+    it('Retorna um objeto', async () => {
+      const {id} = await listModel.create(activityOld);
+      const result = await listModel.update(id, activityNew);
+
+      expect(result).to.be.a('object');
+    });
+
+    it('object possui o id antigo com a atividade atualizada', async () => {
+      const {id} = await listModel.create(activityOld);
+      const result = await listModel.update(id, activityNew);
+
+      expect(result).to.have.a.property('id');
+      expect(result).to.have.a.property('activity');
+    });
+  });
+});
+
+
+describe('Deletar a tarefa no banco de dados', () => {
+  let connectMock;
+
+  const activityOld = 'entrevista amanhã as 15 horas';
+
+  beforeAll(async () => {
+    connectMock = await getConnection();
+    sinon.stub(MongoClient, 'connect').resolves(connectMock);
+  });
+
+  afterAll(async () => {
+    sinon.restore();
+  });
+
+  describe('Quando for um sucesso', () => {
+    it('Retorna um objeto', async () => {
+      const {id} = await listModel.create(activityOld);
+      const result = await listModel.delete(id);
+
+      expect(result).to.be.a('object');
+    });
+
+    it('object possui o id antigo com a atividade atualizada', async () => {
+      const {id} = await listModel.create(activityOld);
+      const result = await listModel.delete(id);
+
+      expect(result).to.have.a.property('id');
+      expect(result).to.have.a.property('activity');
+    });
+  });
+});
